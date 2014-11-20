59ab9c28ae21ced64398881796578f431ad9349f
Anwar Pasaribu <anwargoog@gmail.com>
2014-11-20 18:22:21 +0700

    Variable IP Address
    Guna var ini dalah utk memudahkan dalam penggantian IP pada saat
    membangun aplikasi.

--- src/com/pasaribu/store/model_data/AppsConstanta.java
+++ src/com/pasaribu/store/model_data/AppsConstanta.java
@@ -3,6 +3,8 @@
  */
 package com.pasaribu.store.model_data;
 
+import android.util.Log;
+
 /**
  * Kelas ini berfungsi untuk menyimpan seluruh konstanta yang digunakan pada aplikasi.
  * Sehingga variable pada class lain dapat di ambil dari kelas ini, secara statis.
@@ -11,14 +13,23 @@ package com.pasaribu.store.model_data;
  */
 public class AppsConstanta {
 	
+	private final String TAG = AppsConstanta.class.getSimpleName();
+	
+	public static String MANUAL_IP_ADDRESS = "192.168.56.5";
+	
 	//Header utk setiap JSON Object, respon dari SERVER MySQL
 	public static String JSON_HEADER_BARANG = "BARANG";
 	public static String JSON_HEADER_SUPPLIER = "SUPPLIER";
 	public static String JSON_HEADER_BRAND = "BRAND";
 	public static String JSON_HEADER_DATA_SIZE = "DATA_SIZE";
 	public static String JSON_HEADER_LAST_INSERTED_ID = "LAST_INSERTED_ID";
+	public static String JSON_HEADER_QUERY = "QUERY";
+	public static String JSON_HEADER_MESSAGE = "msg";
 	
-	//
+	//Isi Pesan dari server
+	public static String MESSAGE_SUCCESS = "Berhasil";
+	public static String MESSAGE_FAIL = "Gagal";
+	
 
 	//Informasi Supplier / Penjual
 	public static String 	KEY_NAMA_TOKO = "nama_toko",
@@ -28,13 +39,19 @@ public class AppsConstanta {
 	public static String LAST_INSERTED_ID = "last_inserted_id";
 	
 	//Informasi akses database mysql seperti URL_DATA. 
-	public static String URL_DATA 	= "http://192.168.56.5/pasaribu_store/function/getDataBarang.php"; 
-	public static String URL_INSERT_SUPPLIER 	= "http://192.168.56.5/pasaribu_store/function/insert_supplier.php"; 
-	public static String URL_INSERT_PRODUCT 	= "http://192.168.56.5/pasaribu_store/function/insert_product.php"; 
-	public static String URL_SUPPLIER_AND_BRAND = "http://192.168.56.5/pasaribu_store/function/get_supplier_and_brand.php";  //Ambil data supplier
+	public static String URL_DATA 				= "http://"+ MANUAL_IP_ADDRESS +"/pasaribu_store/function/getDataBarang.php"; 
+	public static String URL_INSERT_SUPPLIER 	= "http://"+ MANUAL_IP_ADDRESS +"/pasaribu_store/function/insert_supplier.php"; 
+	public static String URL_INSERT_PRODUCT 	= "http://"+ MANUAL_IP_ADDRESS +"/pasaribu_store/function/insert_product.php"; 
+	public static String URL_DELETE_PRODUCT 	= "http://"+ MANUAL_IP_ADDRESS +"/pasaribu_store/function/delete_product.php"; 
+	public static String URL_SUPPLIER_AND_BRAND = "http://"+ MANUAL_IP_ADDRESS +"/pasaribu_store/function/get_supplier_and_brand.php";  //Ambil data supplier
 	
 	public AppsConstanta() {
 		// TODO Auto-generated constructor stub
+		Log.i(TAG, "Constructor Apps Constanta Dipanggil");
+	}
+	
+	public void setManualIpAddress(String string_ip_address) {
+		MANUAL_IP_ADDRESS = string_ip_address;
 	}
 
 }
