46b4e28521bf15a49d8e2df3c87e3f8ab6d0753f
Anwar Pasaribu <anwargoog@gmail.com>
2014-11-20 18:28:06 +0700

    Database
    Karena sudah ada pada repositori pasaribu_store

--- MySQL Database/php-function/koneksi.php
+++
@@ -1,1 +0,0 @@
-<?php	$host = "localhost";	$root = "root";	$password = "root";	$db_name = "pasaribu_store";	    $koneksi = mysql_connect($host,$root,$password) or die (mysql_error() . "<br>Gagal koneksi ke server !!");	$database = mysql_select_db($db_name) or die(mysql_error() . "<br>Gagal memilih database !!");	?>
